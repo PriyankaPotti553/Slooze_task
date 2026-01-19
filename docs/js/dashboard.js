f9e6383add9f937aa1747329c19749703955e62e
potti rajkumar <22p31a0553@acet.ac.in>
2026-01-19 09:35:22 +0530

    Backup: snapshot before removing Frontend and moving site to docs

--- docs/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-const loggedIn = localStorage.getItem("loggedIn");
-const role = localStorage.getItem("role");
-
-if (loggedIn !== "true") {
-  window.location.href = "./login.html";
-} else if (role !== "Manager") {
-  alert("Access denied: Managers only");
-  window.location.href = "./products.html";
-}
